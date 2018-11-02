fe710c57e08281a31d9fcfb2163da22ef5838376
Ridhwan <muhammadridhwandaud@gmail.com>
2018-11-02 22:51:28 +0800

    Android build firebase and maps

--- android/app/src/main/java/com/ridex/MainApplication.java
+++ android/app/src/main/java/com/ridex/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,8 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNFirebasePackage(),
-            new MapsPackage()
+          new RNFirebasePackage(),
+          new RNFirebaseAuthPackage(),
+          new MapsPackage()
       );
     }
 
