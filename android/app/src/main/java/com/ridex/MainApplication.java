b28c9490560d1c938cb727350c637bb23efab0ad
Ridhwan <muhammadridhwandaud@gmail.com>
2018-11-14 23:15:59 +0800

    Fixed extra package android

--- android/app/src/main/java/com/ridex/MainApplication.java
+++ android/app/src/main/java/com/ridex/MainApplication.java
@@ -12,7 +12,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.devfd.RNGeocoder.RNGeocoderPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -29,7 +28,6 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNGeocoderPackage(),
           new RNFirebasePackage(),
           new MapsPackage(),
           new RNFirebaseAuthPackage(),
